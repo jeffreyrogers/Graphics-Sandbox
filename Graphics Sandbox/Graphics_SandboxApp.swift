011a9f77361e39fb0f1bb8fc8f4c381623dd09d5
Jeff Rogers <jeffreyrogers27@gmail.com>
2021-08-09 22:25:57 -0700

    Initial Commit

---
+++ Graphics Sandbox/Graphics_SandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Graphics_SandboxApp.swift
+//  Graphics Sandbox
+//
+//  Created by Jeffrey Rogers on 8/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Graphics_SandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
